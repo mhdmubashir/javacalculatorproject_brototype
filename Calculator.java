58219bc2cbf0ac0efaf1ccf2014eccdb3da6b80a
Muhammed Mubashir T <mubashirmuhammed547@gmail.com>
2024-04-15 14:29:05 +0530

    setted jbuttons

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,7 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Calculator {
     public Calculator() {
@@ -12,7 +14,22 @@ public class Calculator {
         displayLabel.setBounds(30, 50, 540, 40);
         displayLabel.setBackground(Color.blue);
         displayLabel.setOpaque(true);
+        displayLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        displayLabel.setForeground(Color.white);
         jf.add(displayLabel);
+        // buttons
+
+        JButton sevenButton = new JButton("7");
+        sevenButton.setBounds(30, 130, 80, 80);
+        jf.add(sevenButton);
+
+        JButton eightButton = new JButton("8");
+        eightButton.setBounds(130, 130, 80, 80);
+        jf.add(eightButton);
+
+        JButton nineButton = new JButton("9");
+        nineButton.setBounds(30, 130, 80, 80);
+        jf.add(nineButton);
 
         jf.setVisible(true);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // window colse cheymbo exitavn
