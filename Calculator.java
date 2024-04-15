5e1274974b9e2a73e72b4f26811f04873456d2f3
Muhammed Mubashir T <mubashirmuhammed547@gmail.com>
2024-04-15 14:11:12 +0530

    created screen

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,14 @@
 import javax.swing.JFrame;
 
 public class Calculator {
-    public Calculator(){
-    JFrame jf=new JFrame("Calculator");
-    jf.set
+    public Calculator() {
+        JFrame jf = new JFrame("Calculator");
+        jf.setLayout(null);
+        jf.setSize(600, 600);
+        jf.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Calculator();
     }
 }
